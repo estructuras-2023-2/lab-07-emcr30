d4cd480b47a67ee164275b0c12ee10607767dead
Evelyn Milagros Chipana Ramos <116284863+emcr30@users.noreply.github.com>
2023-11-03 20:41:41 -0500

    Update studen_file.h

--- studen.file.h
+++ studen.file.h
@@ -1,48 +1,41 @@
 #include <vector>
-using namespace std;
+#include <queue>
 
+using namespace std;
 vector<int> Torneo(vector<int> habilidades, int N, int K) {
-    vector<int> table;
-    vector<int> queue;
-    
-    for (int i = 0; i < K; i++) {
-        int player1 = habilidades[0];
-        int player2 = habilidades[1];
-        
-        if (player1 > player2) {
-            table.push_back(player1);
-            queue.push_back(player2);
+    queue<int> lineup;
+    for (size_t i = 2; i < habilidades.size(); ++i) {
+        lineup.push(habilidades[i]);
+    }
+
+    int gamesWin = 0;
+    int playerActual = habilidades[0];
+    int playerDesafiante = habilidades[1];
+
+
+    vector<int> resul(2);
+    for (int juego = 1; juego <= K; ++juego) {
+        if (playerActual > playerDesafiante) {
+            gamesWin++;
+            resul = {playerDesafiante, playerActual};
+            lineup.push(playerDesafiante);
         } else {
-            table.push_back(player2);
-            queue.push_back(player1);
+            gamesWin = 1;
+            resul = {playerActual, playerDesafiante};
+            lineup.push(playerActual);
+            playerActual = playerDesafiante;
         }
-        
-        habilidades.erase(habilidades.begin());
-        habilidades.erase(habilidades.begin());
-        
-        if (table.size() == 2) {
-            if (table[0] == table[1]) {
-                table.clear();
-            } else {
-                table.push_back(table[1]);
-                table.pop_back();
-            }
+
+        if (gamesWin == N) {
+            lineup.push(playerActual);
+            playerActual = lineup.front();
+            lineup.pop();
+            gamesWin = 0;
         }
-        
-        if (table.size() == 1 && N > 1) {
-            table.push_back(queue[0]);
-            queue.erase(queue.begin());
-            N--;
-        }
-        
-        if (table.size() == 2) {
-            table.clear();
-        }
+
+        playerDesafiante = lineup.front(); 
+        lineup.pop();
     }
     
-    vector<int> result;
-    result.push_back(table[0]);
-    result.push_back(table[1]);
-    
-    return result;
+    return resul;
 }
